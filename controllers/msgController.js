d506ed6cb7d93307e9929e2a7c37ef307548fa3d
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2025-01-27 16:17:14 +0300

    feat: redirect to home after message deleted

--- controllers/msgController.js
+++ controllers/msgController.js
@@ -1,9 +1,25 @@
-import messages from "../models/messages.js";
+import { messages, addMsg, deleteMsg } from "../models/messages.js";
+import ShortUniqueId from "short-unique-id";
+
+const uid = new ShortUniqueId();
 async function getOneMsgById(msgId) {
   return messages.find((msg) => msgId === msg.id);
 }
+
+async function addNewMsg(req, res) {
+  if (req.body.userName !== "") {
+    const newMessage = {
+      id: uid.rnd(),
+      userName: req.body.userName,
+      text: req.body.userMsg,
+      added: new Date(),
+    };
+    addMsg(newMessage);
+    res.redirect("/"); //redirect to home page
+  }
+}
 async function getMsgById(req, res) {
-  const msgId = Number(req.params.id);
+  const msgId = req.params.id;
   const message = await getOneMsgById(msgId);
   if (message) {
     res.render("detail-page", {
@@ -14,17 +30,10 @@ async function getMsgById(req, res) {
     res.render("404", { title: "message not found!" });
   }
 }
-async function delMsgById(req, res) {
-  const msgId = Number(req.params.id);
-  const message = await getOneMsgById(msgId);
-  if (message) {
-    res.render("detail-page", {
-      title: "Message",
-      message: message,
-    });
-  } else {
-    res.render("404", { title: "message not found!" });
-  }
+async function deleteMsgById(req, res) {
+  const msgId = req.params.id;
+  deleteMsg(msgId);
+  res.json({ redirect: "/" });
 }
 
-export { getMsgById };
+export { getMsgById, addNewMsg, deleteMsgById };
