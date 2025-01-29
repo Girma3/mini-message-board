5ead5d413a79687b482be888660f94af846dd62a
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2025-01-29 17:03:39 +0300

    feat: js file to  update time of message realtive to now every minite useing json file from server

---
+++ public/client-side.js
@@ -0,0 +1,41 @@
+console.log("hey");
+// Function to fetch users data from the server
+async function fetchUsers() {
+  //endpoint must matches the server get request
+  const response = await fetch("/api/messages");
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  const users = await response.json();
+  return users;
+}
+// Function to update the DOM with user data
+function updateUsers(users) {
+  const allLi = document.querySelectorAll(".from-current-time");
+  users.forEach((user) => {
+    allLi[users.indexOf(user)].textContent = user.fromCurrentTime;
+  });
+}
+
+// Function to start the real-time updates
+async function startRealTimeUpdate() {
+  try {
+    const users = await fetchUsers(); // Initial fetch when page loads
+    updateUsers(users);
+  } catch (error) {
+    console.error("Failed to fetch users:", error);
+  }
+  // update every minute
+
+  setInterval(async () => {
+    try {
+      const users = await fetchUsers();
+      updateUsers(users);
+    } catch (error) {
+      console.error("Failed to fetch users messages:", error);
+    }
+  }, 60000);
+}
+
+// start update on page load
+document.addEventListener("DOMContentLoaded", startRealTimeUpdate);
